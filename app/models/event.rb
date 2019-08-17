69e5a83d023838ad04aa38ff33b45ef854f24a46
fano <aafano19@gmail.com>
2019-08-17 17:43:25 +0300

    remote

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,32 +2,32 @@ class Event < ApplicationRecord
 belongs_to :admin, class_name: "User"
   has_many :attendances
   has_many :participants , through: :attendances, class_name: "User"
-  validates :start_date ,
-  presence: true 
-  validates :duration,
-  presence: true,
-  numericality: true
-  validate :duration_5?
-  validates :title, 
-  presence: true,
-  length: { in: 5..140 }
-  validates :description, 
-  presence: true,
-  length: {in: 20..1000}
-  validates :price,
-  presence: true,
-  numericality: true,
-  inclusion: { in: 1..1000}
-  validates :location, 
-  presence: true
-  private
-  def duration_5?
-    if duration > 0 && duration % 5 == 0
-      return true
-    else
-      errors.add(:duration, "Le durée doits être positif et multiple de 5")
-      return false
-    end
+  # validates :start_date ,
+  # presence: true 
+  # validates :duration,
+  # presence: true,
+  # numericality: true
+  # validate :duration_5?
+  # validates :title, 
+  # presence: true,
+  # length: { in: 5..140 }
+  # validates :description, 
+  # presence: true,
+  # length: {in: 20..1000}
+  # validates :price,
+  # presence: true,
+  # numericality: true,
+  # inclusion: { in: 1..1000}
+  # validates :location, 
+  # presence: true
+  # private
+  # def duration_5?
+  #   if duration > 0 && duration % 5 == 0
+  #     return true
+  #   else
+  #     errors.add(:duration, "Le durée doits être positif et multiple de 5")
+  #     return false
+  #   end
     
-  end
+  # end
 end
